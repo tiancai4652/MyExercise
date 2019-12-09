d4d0ac2ed25443769ab63045197c43d7a3ec69c6
然 张 <zhangranmm@126.com>
2019-12-09 13:30:45 +0800

    2

---
+++ MyExcercise/AsyncExampleWPF - 副本/Main.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AsyncExampleWPF
+{
+    /// <summary>
+    /// Main.xaml 的交互逻辑
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SynicWindow synicWindow = new SynicWindow();
+            synicWindow.Show();
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            await DemoAsync();
+            MessageBox.Show("Button_Click  ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        async Task DemoAsync()
+        {
+            await Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                MessageBox.Show("DemoAsync.InnerTask ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            });
+            Thread.Sleep(3000);
+            MessageBox.Show("DemoAsync  ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        async void DemoAsync2()
+        {
+            await DemoAsync();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            DemoAsync2();
+            MessageBox.Show("Button_Click  ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+        }
+
+
+        //为什么DemoAsync.InnerTask是一个线程而其他的都是主线程
+    }
+}
---
+++ MyExcercise/AsyncExampleWPF - 副本/SynicWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncExampleWPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class SynicWindow : Window
+    {
+        public SynicWindow()
+        {
+            InitializeComponent();
+        }
+        private async void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Disable the button until the operation is complete.
+            startButton.IsEnabled = false;
+
+            resultsTextBox.Clear();
+
+            // One-step async call.
+            await SumPageSizesAsync();
+
+            // Two-step async call.
+            //Task sumTask = SumPageSizesAsync();
+            //await sumTask;
+
+            resultsTextBox.Text += "\r\nControl returned to startButton_Click.\r\n";
+
+            // Reenable the button in case you want to run the operation again.
+            startButton.IsEnabled = true;
+        }
+
+        private async Task SumPageSizesAsync()
+        {
+            // Make a list of web addresses.
+            List<string> urlList = SetUpURLList();
+
+            var total = 0;
+
+            foreach (var url in urlList)
+            {
+                byte[] urlContents = await GetURLContentsAsync(url);
+                DisplayResults(url, urlContents);
+                total += urlContents.Length;
+            }
+            // Display the total count for all of the websites.
+            resultsTextBox.Text +=
+                $"\r\n\r\nTotal bytes returned:  {total}\r\n";
+        }
+
+        private List<string> SetUpURLList()
+        {
+            List<string> urls = new List<string>
+            {
+                "https://msdn.microsoft.com/library/windows/apps/br211380.aspx",
+                "https://msdn.microsoft.com",
+                "https://msdn.microsoft.com/library/hh290136.aspx",
+                "https://msdn.microsoft.com/library/ee256749.aspx",
+                "https://msdn.microsoft.com/library/hh290138.aspx",
+                "https://msdn.microsoft.com/library/hh290140.aspx",
+                "https://msdn.microsoft.com/library/dd470362.aspx",
+                "https://msdn.microsoft.com/library/aa578028.aspx",
+                "https://msdn.microsoft.com/library/ms404677.aspx",
+                "https://msdn.microsoft.com/library/ff730837.aspx"
+            };
+            return urls;
+        }
+
+        bool taskx = false;
+        public bool Taskx 
+        {
+            get
+            {
+                return taskx;
+            }
+            set
+            {
+                taskx = value;
+            }
+        }
+
+        private async Task<byte[]> GetURLContentsAsync(string url)
+        {
+            // The downloaded resource ends up in the variable named content.
+            var content = new MemoryStream();
+
+            //Task task = Task.Delay(1000 * 5);
+            //Task<bool> task = new Task(() => { Thread.Sleep(1000 * 5);return null; });
+
+            Task<bool> task= MyDelayAsynic(1000 );
+            Taskx = await task;
+
+            // Return the result as a byte array.
+            return content.ToArray();
+        }
+
+        private async Task<bool> MyDelayAsynic(int ms)
+        {
+            await Task.Delay(ms);
+            return true;
+        }
+
+
+
+
+
+        private void DisplayResults(string url, byte[] content)
+        {
+            // Display the length of each website. The string format
+            // is designed to be used with a monospaced font, such as
+            // Lucida Console or Global Monospace.
+            var bytes = content.Length;
+            // Strip off the "https://".
+            var displayURL = url.Replace("https://", "");
+            resultsTextBox.Text += $"\n{displayURL,-58} {bytes,8}";
+        }
+    }
+}
