ddbb536115783ba72409e89252982014dcd4b920
然 张 <zhangranmm@126.com>
2019-12-09 13:45:06 +0800

    11

---
+++ MyExcercise/AsyncExampleWPF/GetByteArrayAsync.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncExampleWPF
+{
+    public partial class GetByteArrayAsync : Window
+    {
+        public GetByteArrayAsync()
+        {
+            InitializeComponent();
+        }
+
+        private async void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultsTextBox.Clear();
+
+            // One-step async call.
+            await SumPageSizesAsync();
+
+            // Two-step async call.
+            //Task sumTask = SumPageSizesAsync();
+            //await sumTask;
+
+            resultsTextBox.Text += "\r\nControl returned to startButton_Click.\r\n";
+        }
+
+        private async Task SumPageSizesAsync()
+        {
+            // Make a list of web addresses.
+            List<string> urlList = SetUpURLList();
+
+            // Declare an HttpClient object and increase the buffer size. The
+            // default buffer size is 65,536.
+            HttpClient client = new HttpClient() { MaxResponseContentBufferSize = 1000000 };
+
+            // Create a query.
+            IEnumerable<Task<int>> downloadTasksQuery =
+                from url in urlList select ProcessURLAsync(url, client);
+
+            // Use ToArray to execute the query and start the download tasks.
+            Task<int>[] downloadTasks = downloadTasksQuery.ToArray();
+
+            // You can do other work here before awaiting.
+
+            // Await the completion of all the running tasks.
+            int[] lengths = await Task.WhenAll(downloadTasks);
+
+            //// The previous line is equivalent to the following two statements.
+            //Task<int[]> whenAllTask = Task.WhenAll(downloadTasks);
+            //int[] lengths = await whenAllTask;
+
+            int total = lengths.Sum();
+
+            //var total = 0;
+            //foreach (var url in urlList)
+            //{
+            //    // GetByteArrayAsync returns a Task<T>. At completion, the task
+            //    // produces a byte array.
+            //    byte[] urlContent = await client.GetByteArrayAsync(url);
+
+            //    // The previous line abbreviates the following two assignment
+            //    // statements.
+            //    Task<byte[]> getContentTask = client.GetByteArrayAsync(url);
+            //    byte[] urlContent = await getContentTask;
+
+            //    DisplayResults(url, urlContent);
+
+            //    // Update the total.
+            //    total += urlContent.Length;
+            //}
+
+            // Display the total count for all of the web addresses.
+            resultsTextBox.Text +=
+                $"\r\n\r\nTotal bytes returned:  {total}\r\n";
+        }
+
+        private List<string> SetUpURLList()
+        {
+            List<string> urls = new List<string>
+            {
+                       "https://www.cnblogs.com/lsq6/p/12006839.html",
+        "https://www.cnblogs.com/MaMaNongNong/p/11945161.html",
+        "https://www.cnblogs.com/",
+        "https://www.cnblogs.com/xibei/p/12001805.html",
+        "https://www.cnblogs.com/haogj/p/12003854.html",
+            };
+            return urls;
+        }
+
+        // The actions from the foreach loop are moved to this async method.
+        async Task<int> ProcessURLAsync(string url, HttpClient client)
+        {
+            byte[] byteArray = await client.GetByteArrayAsync(url);
+            DisplayResults(url, byteArray);
+            return byteArray.Length;
+        }
+
+        private void DisplayResults(string url, byte[] content)
+        {
+            // Display the length of each web site. The string format
+            // is designed to be used with a monospaced font, such as
+            // Lucida Console or Global Monospace.
+            var bytes = content.Length;
+            // Strip off the "https://".
+            var displayURL = url.Replace("https://", "");
+            resultsTextBox.Text += $"\n{displayURL,-58} {bytes,8}";
+        }
+    }
+}
---
+++ MyExcercise/AsyncExampleWPF/GetURLContentsAsync.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace AsyncExampleWPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class GetURLContentsAsync : Window
+    {
+        public GetURLContentsAsync()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultsTextBox.Clear();
+            SumPageSizes();
+            resultsTextBox.Text += "\r\nControl returned to startButton_Click.";
+        }
+
+        private void SumPageSizes()
+        {
+            // 获取网址列表
+            List<string> urlList = SetUpURLList();
+
+            var total = 0;
+            foreach (var url in urlList)
+            {
+                // GetURLContents returns the contents of url as a byte array.
+                byte[] urlContents = GetURLContents(url);
+
+                DisplayResults(url, urlContents);
+
+                // Update the total.
+                total += urlContents.Length;
+            }
+
+            // Display the total count for all of the web addresses.
+            resultsTextBox.Text += $"\r\n\r\nTotal bytes returned:  {total}\r\n";
+        }
+
+        private List<string> SetUpURLList()
+        {
+            var urls = new List<string>
+    {
+        "https://www.cnblogs.com/lsq6/p/12006839.html",
+        "https://www.cnblogs.com/MaMaNongNong/p/11945161.html",
+        "https://www.cnblogs.com/",
+        "https://www.cnblogs.com/xibei/p/12001805.html",
+        "https://www.cnblogs.com/haogj/p/12003854.html",
+    };
+            return urls;
+        }
+
+        private byte[] GetURLContents(string url)
+        {
+            // The downloaded resource ends up in the variable named content.
+            var content = new MemoryStream();
+
+            // Initialize an HttpWebRequest for the current URL.
+            var webReq = (HttpWebRequest)WebRequest.Create(url);
+
+            //// Send the request to the Internet resource and wait for
+            //// the response.
+            //// Note: you can't use HttpWebRequest.GetResponse in a Windows Store app.
+            //using (WebResponse response = webReq.GetResponse())
+            //{
+            //    // Get the data stream that is associated with the specified URL.
+            //    using (Stream responseStream = response.GetResponseStream())
+            //    {
+            //        // Read the bytes in responseStream and copy them to content.
+            //        responseStream.CopyTo(content);
+            //    }
+            //}
+            //模拟网站响应时间为500ms
+            Thread.Sleep(500);
+
+            // Return the result as a byte array.
+            return content.ToArray();
+        }
+
+        private void DisplayResults(string url, byte[] content)
+        {
+            // Display the length of each website. The string format
+            // is designed to be used with a monospaced font, such as
+            // Lucida Console or Global Monospace.
+            var bytes = content.Length;
+            // Strip off the "https://".
+            var displayURL = url.Replace("https://", "");
+            resultsTextBox.Text += $"\n{displayURL,-58} {bytes,8}";
+        }
+    }
+}
