25ccc5708f77bd101b857c49956c38945ff3aa62
然 张 <zhangranmm@126.com>
2019-12-10 09:14:12 +0800

    11

--- MyExcercise/WpfApp1/MainWindow.xaml.cs
+++ MyExcercise/WpfApp1/MainWindow.xaml.cs
@@ -31,21 +31,25 @@ namespace WpfApp1
         static long Size = 10000;
         static string MutexNmae = "testmapmutex";
 
+
+        /// <summary>
+        /// 只有Creat和Dispose用
+        /// </summary>
+        static MemoryMappedFile MemoryMappedFile;
+        static Mutex Mutex;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            using (MemoryMappedFile mmf = MemoryMappedFile.CreateNew(MemoryMappedFileNmae, Size))
+            MemoryMappedFile = MemoryMappedFile.CreateNew(MemoryMappedFileNmae, Size);
+            bool mutexCreated;
+            Mutex = new Mutex(true, MutexNmae, out mutexCreated);
+            using (MemoryMappedViewStream stream = MemoryMappedFile.CreateViewStream())
             {
-                bool mutexCreated;
-                Mutex mutex = new Mutex(true, MutexNmae, out mutexCreated);
-                using (MemoryMappedViewStream stream = mmf.CreateViewStream())
-                {
-                    BinaryWriter writer = new BinaryWriter(stream);
-                    writer.Write(1);
-                }
-                mutex.ReleaseMutex();
-
-                txtbox.Text += "MemoryMappedFile.CreateNew" + Environment.NewLine;
+                BinaryWriter writer = new BinaryWriter(stream);
+                writer.Write(1);
             }
+            Mutex.ReleaseMutex();
+            txtbox.Text += "MemoryMappedFile.CreateNew" + Environment.NewLine;
+            txtbox.Text += "writer.Write(1)" + Environment.NewLine;
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -67,7 +71,7 @@ namespace WpfApp1
                     txtbox.Text += "writer.Write(0)" + Environment.NewLine;
                 }
             }
-            catch (FileNotFoundException)
+            catch (Exception ex)
             {
                 Console.WriteLine("Memory-mapped file does not exist. Run Process A first.");
                 txtbox.Text += "Memory - mapped file does not exist. Run Process A first." + Environment.NewLine;
@@ -93,7 +97,7 @@ namespace WpfApp1
                     txtbox.Text += "writer.Write(1)" + Environment.NewLine;
                 }
             }
-            catch (FileNotFoundException)
+            catch (Exception ex)
             {
                 Console.WriteLine("Memory-mapped file does not exist. Run Process A first, then B.");
                 txtbox.Text += "Memory - mapped file does not exist. Run Process A first." + Environment.NewLine;
@@ -120,7 +124,7 @@ namespace WpfApp1
                     
                 }
             }
-            catch (FileNotFoundException)
+            catch (Exception ex)
             {
                 Console.WriteLine("Memory-mapped file does not exist. Run Process A first, then B.");
                 txtbox.Text += "Memory - mapped file does not exist. Run Process A first." + Environment.NewLine;
@@ -131,21 +135,11 @@ namespace WpfApp1
         {
             try
             {
-                using (MemoryMappedFile mmf = MemoryMappedFile.OpenExisting(MemoryMappedFileNmae))
-                {
-                    Mutex mutex = Mutex.OpenExisting(MutexNmae);
-                    mutex.WaitOne();
-                    using (MemoryMappedViewStream stream = mmf.CreateViewStream())
-                    {
-                        BinaryReader reader = new BinaryReader(stream);
-                        txtbox.Text += $"Process A says: { reader.ReadBoolean()}" + Environment.NewLine;
-                        txtbox.Text += $"Process B says: { reader.ReadBoolean()}" + Environment.NewLine;
-                        txtbox.Text += $"Process C says: { reader.ReadBoolean()}" + Environment.NewLine;
-                    }
-                    mutex.ReleaseMutex();
-                }
+                MemoryMappedFile.Dispose();
+                Mutex.Dispose();
+                txtbox.Text += "MemoryMappedFile and Mutex.Dispose();" + Environment.NewLine;
             }
-            catch (FileNotFoundException)
+            catch (Exception ex)
             {
                 Console.WriteLine("Memory-mapped file does not exist. Run Process A first, then B.");
                 txtbox.Text += "Memory - mapped file does not exist. Run Process A first." + Environment.NewLine;
